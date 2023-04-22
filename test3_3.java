576d5f62a312e4f92ace9df0e6e9c143702a539c
mocchio <skmt.rome@gmail.com>
2023-04-22 15:52:04 +0900

    条件分岐練習

---
+++ test3_3.java
@@ -0,0 +1,14 @@
+public class Main {
+  public static void main(String[] args) {
+    int isHungry = 1;
+    String food = "バナナ";
+    System.out.println("こんにちは");
+    if (isHungry == 0) {
+      System.out.println("お腹いっぱいです");
+    } else {
+      System.out.println("腹ペコです");
+      System.out.println(food + "をいただきます");
+    }
+    System.out.println("ごちそうさまでした");
+  }
+}
