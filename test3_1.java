0e84b191bdb474203d94235c9ead4e2179e1c7f7
mocchio <skmt.rome@gmail.com>
2023-04-22 15:39:00 +0900

    条件式練習

---
+++ test3_1.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    // 問題①
+    weight == 60
+
+    // 問題②
+    (age1 + age2) * 2 > 60
+
+    // 問題③
+    age % 2 == 1
+
+    // 問題④
+    name.equals("湊")
+  }
+}
