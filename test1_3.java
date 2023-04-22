c46aa4ade8fa06908ebcdcfe745228a2052e5fcd
mocchio <skmt.rome@gmail.com>
2023-04-22 14:52:18 +0900

    Initial commit

---
+++ test1_3.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    // 型のテスト
+    boolean isTrue = true;
+    char content = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String dialogue = "ミナトの攻撃";
+    System.out.println(dialogue);
+  }
+}
---
+++ test2_1.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 10;
+    int z = x + y;
+    String ans = "x+yは" + z;
+    System.out.println(ans);
+  }
+}
