e89d9cd79e3b07f8b883e5dddc1c0121f94f63da
mocchio <skmt.rome@gmail.com>
2023-04-24 18:19:33 +0900

    selfAidメソッドの記述

--- practice7_5/Cleric.java
+++ practice7_5/Cleric.java
@@ -4,4 +4,11 @@ public class Cleric {
   final int maxHp = 50;
   int MP = 10;
   final int maxMp = 10;
+
+  public void selfAid() {
+    System.out.println(this.name + "は、セルフエイドを唱えた！");
+    this.Mp -= 5;
+    this.Hp = this.maxHp;
+    System.out.println("HPが最大まで回復した！");
+  }
 }
