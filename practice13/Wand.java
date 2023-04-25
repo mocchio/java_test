acffdcb35ba4628e5eeb3a30d5d36cfb20a32421
mocchio <skmt.rome@gmail.com>
2023-04-25 18:06:17 +0900

    setterの妥当性の検証

--- practice13/Wand.java
+++ practice13/Wand.java
@@ -4,7 +4,20 @@ public class Wand {
   
   // getterとsetterの設定
   public String getName() { return this.name; }
-  public void setName(String name) { return this.name = name; }
+  public void setName(String name) {
+    if ( name == null || name.length() < 3) {
+      throw new IllegalArgument Exception
+      ("杖の名前が異常です");
+    }
+    this.name = name;
+  }
+
   public double getPower() { return this.power; }
-  public void setPower(double power) { return this.power = power; }
+  public void setPower(double power) {
+    if (power < 0.5 || power > 100.0) {
+      throw new IllegalArgument Exception
+      ("杖による魔力が異常です");
+    }
+    this.power = power;
+  }
 }
--- practice13/Wizard.java
+++ practice13/Wizard.java
@@ -15,11 +15,38 @@ public class Wizard {
 
   // getterとsetterの設定
   public int getHp() { return this.hp; }
-  public void setHp(int hp) { return this.hp = hp; }
+  public void setHp(int hp) {
+    if (hp < 0) {
+      this.hp = 0;
+    } else {
+      this.hp = hp;
+    }
+  }
+
   public int getMp() { return this.mp; }
-  public void setMp(int mp) { return this.mp = mp; }
+  public void setMp(int mp) {
+    if (mp < 0) {
+      throw new IllegalArgument Exception
+      ("MPは0以上でなければいけない");
+    }
+    this.mp = mp;
+  }
+
   public String getName() { return this.name; }
-  public void setName(String name) { return this.name = name; }
+  public void setName(String name) {
+    if ( name == null || name.length < 3) {
+      throw new IllegalArgument Exception
+      ("魔法使いの名前が異常です");
+    }
+    this.name = name;
+  }
+
   public Wand getWand() { return this.wand; }
-  public void setHp(Wand wand) { return this.wand = wand; }
+  public void setHp(Wand wand) {
+    if (wand == null) {
+      throw new IllegalArgument Exception
+      ("杖は必ず装備しなければならない");
+    }
+    this.wand = wand;
+  }
 }
