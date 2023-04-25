cad170f1797cd85d7a0579c772ce64e7da37bf8d
mocchio <skmt.rome@gmail.com>
2023-04-25 17:46:40 +0900

    getterとsetterの設定

--- practice13/Wand.java
+++ practice13/Wand.java
@@ -1,4 +1,10 @@
 public class Wand {
   private String name;
   private double power;
+  
+  // getterとsetterの設定
+  public String getName() { return this.name; }
+  public void setName(String name) { return this.name = name; }
+  public double getPower() { return this.power; }
+  public void setPower(double power) { return this.power = power; }
 }
--- practice13/Wizard.java
+++ practice13/Wizard.java
@@ -8,8 +8,18 @@ public class Wizard {
   // メソッドのアクセス制御
   public void heal(Hero h) {
     int basePoint = 10; // 基本回復ポイント
-    int recovPoint = (int)(basePoint * this.wond.power); // 杖による回復ポイント
+    int recovPoint = (int)(basePoint * this.getWand.getPower); // 杖による回復ポイント
     h.setHp(h.getHp() + recovPoint); // 勇者のHPを回復する
     System.out.println(h.getName() + "のHPを" + recovPoint + "回復した！");
   }
+
+  // getterとsetterの設定
+  public int getHp() { return this.hp; }
+  public void setHp(int hp) { return this.hp = hp; }
+  public int getMp() { return this.mp; }
+  public void setMp(int mp) { return this.mp = mp; }
+  public String getName() { return this.name; }
+  public void setName(String name) { return this.name = name; }
+  public Wand getWand() { return this.wand; }
+  public void setHp(Wand wand) { return this.wand = wand; }
 }
