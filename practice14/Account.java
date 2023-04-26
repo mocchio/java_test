21cac0bb981e8a01a9fd2b8203cb42a06a4fa545
mocchio <skmt.rome@gmail.com>
2023-04-26 16:36:33 +0900

    等価判定メソッド練習

--- practice14/Account.java
+++ practice14/Account.java
@@ -6,4 +6,20 @@ public class Account {
   public String toString() {
     return "¥" + this.balance + "(口座番号：" + this.accountNumber + ")";
   }
+
+  // 等価判定メソッド
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o instanceof Account) {
+      Account a = (Account)o;
+      String an1 = this.accountNumber.trim();
+      String an2 = a.accountNumber.trim();
+      if (an1.equals(an2)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
--- practice14/Main.java
+++ practice14/Main.java
@@ -1,8 +1,12 @@
 public class Main {
   public static void main(String[] args) {
-    Account x = new Account();
-    x.accountNumber = "4649";
-    x.balance = 1592;
-    System.out.println(x.toString());
+    Account a = new Account();
+    a.accountNumber = "4649";
+    a.balance = 1592;
+    //System.out.println(a.toString());
+    Account o = new Account();
+    o.accountNumber = " 4649";
+    o.balance = 1592;
+    System.out.println(a.equals(o));
   }
 }
