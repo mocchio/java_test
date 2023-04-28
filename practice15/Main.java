7b018837cd3b59c45a0c632dec0663d184fc7f1c
mocchio <skmt.rome@gmail.com>
2023-04-28 17:04:47 +0900

    文字列操作を用いた連結

--- practice15/Main.java
+++ practice15/Main.java
@@ -2,12 +2,25 @@ import java.util.Arrays;
 
 public class Main {
   public static void main(String[] args) {
-    StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < 100; i++) {
-      sb.append(i+1).append(",");
+    // ①StringBuilderを用いた連結
+    // StringBuilder sb = new StringBuilder();
+    // for (int i = 0; i < 100; i++) {
+    //   sb.append(i+1).append(",");
+    // }
+    // String s = sb.toString();
+    // String[] a = s.split(",");
+    // System.out.println(Arrays.toString(a));
+
+    // ②文字列操作
+    String folder = "c:¥java¥";
+    String file = "test.java";
+    System.out.println(concatPath(folder, file));
+  }
+  // ②文字列操作メソッド
+  public static String concatPath(String folder, String file) {
+    if (!folder.endsWith("¥")) {
+      folder += "¥";
     }
-    String s = sb.toString();
-    String[] a = s.split(",");
-    System.out.println(Arrays.toString(a));
+    return folder + file;
   }
 }
