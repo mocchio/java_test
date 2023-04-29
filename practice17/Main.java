fd617fe84257f67d5906db56d12204b4ec94dcd0
mocchio <skmt.rome@gmail.com>
2023-04-29 15:02:49 +0900

    IntegerクラスのparseIntメソッドの例外処理

--- practice17/Main.java
+++ practice17/Main.java
@@ -3,14 +3,22 @@ import java.io.*;
 public class Main {
   public static void main(String[] args) {
     // try-catch文の記述
+    // try {
+    //   String s = null;
+    //   System.out.println(s.length());
+    // } catch (NullPointerException e) {
+    //   System.out.println("NullPointerException例外をcatchしました");
+    //   System.out.println("--スタックトレース（ここから）--");
+    //   e.printStackTrace();
+    //   System.out.println("--スタックトレース（ここまで）--");
+    // }
+
+    // NumberFormatException
     try {
-      String s = null;
-      System.out.println(s.length());
-    } catch (NullPointerException e) {
-      System.out.println("NullPointerException例外をcatchしました");
-      System.out.println("--スタックトレース（ここから）--");
-      e.printStackTrace();
-      System.out.println("--スタックトレース（ここまで）--");
+      String n = "三";
+      int num = Integer.parseInt(n);
+    } catch (NumberFormatException e) {
+      System.out.println("NumberFormatException例外が発生しました");
     }
   }
 }
